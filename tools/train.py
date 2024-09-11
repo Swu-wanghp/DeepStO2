8814d9a56d0038e6cbdcb99ea18d192a574b3a37
Swu-wanghp <wanghp568@gmail.com>
2024-09-11 17:40:56 +0800

    2024.9.11 优化了训练速度等

--- tools/train.py
+++ tools/train.py
@@ -1,5 +1,5 @@
 import os
-import tqdm
+import sys
 import time
 import shutil
 import torch
@@ -12,15 +12,30 @@ from model import Cnn6
 from model import MobileNetV2
 from datasets import StO2Dataset, transformer
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+sys.path.append("..")
+
+# 打开文件并写入数据，覆盖已有内容
+with open('output.txt', 'w') as f:
+    f.write("Hello, this is a test.\n")
+    f.write("This is the second line.\n")
 
 
 # 进行LOSO训练与测试
 def train(agrs):
     # 用于计算准确率平均值
     averageAll = 0
+    # outputPath 为中间输出保存路径,命名方式：train_batchsize  lr
+    outputFilename = "results" + "trainBatchsize" + "_" + agrs.train_batchsize + "_" + agrs.lr + ".txt"
+    outputPath = os.path.join("./results", outputFilename)
+    if not os.path.exists(outputPath):
+        with open(outputPath, "w") as f:
+            pass
+
+
     # 对每一个被试进行训练
     for p in range(args.sub_num):
-
+        # p += 15
         # 初始化变量， 暂时存储最大值，训练损失函数
         temp_max = 0
         train_loss = 0
@@ -28,19 +43,20 @@ def train(agrs):
         print("当前遍历第{}个人".format(p + 1))
         train_dataset = StO2Dataset(agrs.root_path, train=True, test_subnum=p + 1, transform=transformer.transform())
         test_datast = StO2Dataset(agrs.root_path, train=False, test_subnum=p + 1, transform=transformer.transform())
-        train_loader = DataLoader(dataset=train_dataset, batch_size=args.train_batchsize, shuffle=True)
-        val_loader = DataLoader(dataset=test_datast, batch_size=args.test_batchsize, shuffle=False)
+        train_loader = DataLoader(dataset=train_dataset, batch_size=args.train_batchsize, num_workers=16, shuffle=True)
+        val_loader = DataLoader(dataset=test_datast, batch_size=args.test_batchsize, num_workers=16, shuffle=False)
 
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         model = Cnn6()
         model.to(device)
         model.zero_grad()
-        optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.9)
+        optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=0.9)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
 
         # 每个被试下epoch
         for k in range(args.train_epoch):
             model.train()
-            for batch in train_loader:
+            for batch in tqdm(train_loader):
                 images, labels = batch
                 images = images.to(device)
                 labels = labels.to(device)
@@ -51,7 +67,7 @@ def train(agrs):
                 loss = F.cross_entropy(preds, labels)
                 loss.backward()
                 optimizer.step()
-                optimizer.zero_grad()
+                scheduler.step()
                 print("epoch: {} | loss = {}".format(k + 1, loss))
 
                 train_loss += loss.item()
@@ -72,8 +88,10 @@ def train(agrs):
                     preds.append(predict)
                 labels = torch.cat(labels, dim=0)
                 preds = torch.cat(preds, dim=0)
-                labels = labels.cpu().numpy()
-                preds = preds.cpu().numpy()
+                labels = labels.to('cpu').numpy()
+                preds = preds.to('cpu').numpy()
+                print("labels = ", labels)
+                print("preds = ", preds)
                 eval_result = (np.sum(labels == preds)) / len(labels)
                 print("第{}个被试的第{}周期下的预测结果为{}".format(p + 1, k + 1, eval_result))
                 # 在一个epoch下，如果acc值大于当前的，则保存该模型
